468c8f5f1885873836cea5471673c500b0721036
Kavindu Shyamal <shyamald_im17073@stu.kln.ac.lk>
2022-05-21 19:49:43 +0530

    Adding routing to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { Route } from "react-router-dom";
 import Movies from "./components/movie";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notfound";
+import { Switch } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/notfound" component={NotFound}></Route>
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/notfound" />
+      </Switch>
     </div>
   );
 }
