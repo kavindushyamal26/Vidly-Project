5fda69cee7eecff2120fa83393ba5769ae907e26
Kavindu Shyamal <shyamald_im17073@stu.kln.ac.lk>
2022-05-21 20:07:37 +0530

    Initiating navbar component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notfound";
 import { Switch } from "react-router-dom";
 import { Redirect } from "react-router-dom";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/notfound" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/notfound" />
-      </Switch>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/notfound" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/notfound" />
+        </Switch>
+      </div>
+    </>
   );
 }
 
