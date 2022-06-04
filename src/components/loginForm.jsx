2b0ffe8d839e7a3ec8581471edcd160d2b8edbe7
Kavindu Shyamal <shyamald_im17073@stu.kln.ac.lk>
2022-06-04 17:33:31 +0530

    Prevent default and autofocus

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
-const LoginForm = () => {
-  return (
-    <div>
-      <h1>Login Form</h1>
-      <form>
-        <div className="form-group my-2">
-          <label htmlFor="username">Username</label>
-          <input
-            type="email"
-            className="form-control"
-            id="username"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="form-group my-2">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary my-3">
-          Login
-        </button>
-      </form>
-    </div>
-  );
-};
+class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  render() {
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group my-2">
+            <label htmlFor="username">Username</label>
+            <input
+              autoFocus
+              type="email"
+              className="form-control"
+              id="username"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group my-2">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary my-3">
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default LoginForm;
